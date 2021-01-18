927b85ec0e315d1f23a69849b3aef90ccb4950a1
RajatYadav1908 <73642006+RajatYadav1908@users.noreply.github.com>
2021-01-18 14:48:50 +0530

    Add files via upload

---
+++ detectCollision.js
@@ -0,0 +1,13 @@
+function detectCollision(lstone,lmango){
+
+    mangoBodyPosition=lmango.body.position
+    stoneBodyPosition=lstone.body.position
+
+    var distance=dist(stoneBodyPosition.x, stoneBodyPosition.x, mangoBodyPosition.x, mangoBodyPosition.y)
+    //console.log(distance);
+    //console.log(lmango.r,lstone.r)
+    if(distance<=lmango.r+lstone.r)
+    {
+        Matter.Body.setStatic(lmango.body,false);
+    }
+}
